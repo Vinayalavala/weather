bcfdf71776810e4b7770adf190f8afc338d58876
Vinayalavala <147227489+Vinayalavala@users.noreply.github.com>
2023-10-07 16:10:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const apikey="ba707aac3c6c866490c9f22ada735ba3";
+const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchbox=document.querySelector(".search input")
+const searchbutton=document.querySelector(".search button")
+const weatherIcon=document.querySelector(".weather-icon")
+
+async function checkweather(city){
+    const response=await fetch(apiurl + city+`&appid=${apikey}`);
+
+    if(response.status==404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+    }
+    else{
+        var data=await response.json()
+        document.querySelector(".city").innerHTML=data.name
+        document.querySelector(".temp").innerHTML=Math.round(data.main.temp) + "°c";
+        document.querySelector(".humidity").innerHTML=data.main.humidity  + "%";
+        document.querySelector(".wind").innerHTML=data.wind.speed + "KM/H";
+    
+        if(data.weather[0].main=="Clouds"){
+            weatherIcon.src="cloudy.png"
+        }
+        if(data.weather[0].main=="Clear"){
+            weatherIcon.src="clear.png"
+        }
+        if(data.weather[0].main=="Clouds"){
+            weatherIcon.src="cloudy.png"
+        }
+        if(data.weather[0].main=="Rain"){
+            weatherIcon.src="rain.png"
+        }
+        if(data.weather[0].main=="Drizzle"){
+            weatherIcon.src="drizzle.png"
+        }
+        if(data.weather[0].main=="Mist"){
+            weatherIcon.src="mist.png"
+        }
+        document.querySelector("weather").style.display="block"
+        document.querySelector(".error").style.display="none";
+    }
+
+   
+}
+
+searchbutton.addEventListener("click",()=>{
+    checkweather(searchbox.value)
+})
+
+checkweather()
